3209542ea18570cb744831fd07c9143a24fc4b64
Nikita Ilin <i.am.rock@mail.ru>
2020-03-31 22:13:09 +0100

    fixing chart

--- templates/script.js
+++ templates/script.js
@@ -1,70 +1,71 @@
-	var timeFormat = 'MM/DD/YYYY HH:mm';
+var timeFormat = 'MM/DD/YYYY HH:mm';
 
 
 
-		function newDateLabel(hours) {
-			return moment().add(hours, 'h').toDate();
-		}
+function newDateLabel(hours) {
+    return moment().add(hours, 'h').toDate();
+}
 
-		function newDate(sec) {
-			return moment().add(sec, 'd').toDate();
-		}
+function newDate(sec) {
+    return moment().add(sec, 'd').toDate();
+}
 
-		function newDateString(sec) {
-			return moment(sec*1000).format(timeFormat);
-		}
+function newDateString(sec) {
+    return moment(sec*1000).format(timeFormat);
+}
 
-		var color = Chart.helpers.color;
-		var config = {
-			type: 'line',
-			data: {
-				labels: [ // Date Objects
-				],
-				datasets: [ {
-					label: 'Температура процессора',
-					backgroundColor: color('#bbcc88').alpha(0.7).rgbString(),
-					borderColor: "#bbcc88",
-					fill: false,
-                    data : [{% for item in cache['graphs']['cpu_temp'] %} { x: newDateString({{item}}), y: {{ cache['graphs']['cpu_temp'][item]}} },{% endfor %}],
-				},/*{
-					label: 'Место на диске',
-					backgroundColor: color('#dd8888').alpha(0.7).rgbString(),
-					borderColor: "#dd8888",
-					fill: false,
-                    data : [{% for item in cache['graphs']['diskspace_left'] %} { x: newDateString({{item}}), y: {{ cache['graphs']['diskspace_left'][item]}} },{% endfor %}],
-				}*/]
-			},
-			options: {
-				title: {
-					text: 'Chart.js Time Scale'
-				},
-				scales: {
-					xAxes: [{
-						type: 'time',
-						time: {
-							parser: timeFormat,
-							tooltipFormat: 'll HH:mm'
-						},
-						scaleLabel: {
-							display: true,
-							labelString: 'Date'
-						}
-					}],
-					yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        },
-						scaleLabel: {
-							display: true,
-							labelString: 'value'
-						}
-					}]
-				},
-			}
-		};
+var color = Chart.helpers.color;
+var config = {
+    type: 'line',
+    data: {
+        labels: [ // Date Objects
+        ],
+        datasets: [ {
+            label: 'Температура процессора',
+            backgroundColor: color('#bbcc88').alpha(0.7).rgbString(),
+            borderColor: "#bbcc88",
+            fill: false,
+            data : [{% for item in cache['graphs']['cpu_temp'] %} { x: newDateString({{item}}), y: {{ cache['graphs']['cpu_temp'][item]}} },{% endfor %}],
+        },/*{
+            label: 'Место на диске',
+            backgroundColor: color('#dd8888').alpha(0.7).rgbString(),
+            borderColor: "#dd8888",
+            fill: false,
+            data : [{% for item in cache['graphs']['diskspace_left'] %} { x: newDateString({{item}}), y: {{ cache['graphs']['diskspace_left'][item]}} },{% endfor %}],
+        }*/]
+    },
+    options: {
+        title: {
+            text: 'Chart.js Time Scale'
+        },
+        maintainAspectRatio: false,
+        scales: {
+            xAxes: [{
+                type: 'time',
+                time: {
+                    parser: timeFormat,
+                    tooltipFormat: 'll HH:mm'
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Date'
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'value'
+                }
+            }]
+        },
+    }
+};
 
-		window.onload = function() {
-			var ctx = document.getElementById('canvas').getContext('2d');
-			window.myLine = new Chart(ctx, config);
+window.onload = function() {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    window.myLine = new Chart(ctx, config);
 
-		};
+};
